9d337a72b7234e0c5f7eac0e938f7f20e5c3a59a
Daniel Ng <ngenator@gmail.com>
2013-05-27 16:47:41 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='GuildWars2API',
+    version='1.0',
+    packages=['guildwars2api'],
+    url='https://github.com/ngenator/guildwars2api',
+    license='Creative Commons Attribution-ShareAlike 3.0 Unported License',
+    author='Daniel Ng',
+    author_email='ngenator@gmail.com',
+    description='Guild Wars 2 API wrapper for Python'
+)
