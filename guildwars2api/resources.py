0db6d7d74ef7b8165309bebdf8b54e226c5d2161
Daniel Ng <ngenator@gmail.com>
2013-08-23 14:38:24 -0400

    Fix comment

--- guildwars2api/resources.py
+++ guildwars2api/resources.py
@@ -44,7 +44,7 @@ class Resource(object):
 
     def get(self, **kwargs):
         """
-        Get the data from the API and populate the current object with the values
+        Get the data from the API and return the values
         """
 
         url = self._build_url(**kwargs)
